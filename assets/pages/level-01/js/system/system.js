14435a692e5d0223f4be524e046239debaf98ff8
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-08-31 17:35:39 -0300

    update: melhoramento de código; resolução de bugs

--- assets/pages/level-01/js/system/system.js
+++ assets/pages/level-01/js/system/system.js
@@ -17,265 +17,137 @@ let res_dices = document.querySelector('#res-dices')
 
 let btn_stats = [...document.querySelectorAll('.btn-stats')]
 
-let btn_options = [...document.querySelectorAll('.btn-options')
-]
+let btn_options = [...document.querySelectorAll('.btn-options')]
 
 let alertShown = true
 
-// btn_stats.map((el)=>{
-//     el.addEventListener('click', (evt)=>{
-//         el.classList.toggle('selected')
-//     })
-// })
-
 let card
+
+let currentAction = ''
+
+// Captura o id da carta
 const capturarId = (evt) => {
     card = evt.currentTarget.id
     console.log(card)
 }
 
-document.getElementById("btn-hacker").addEventListener("click", capturarId);
-document.getElementById("btn-staff").addEventListener("click", capturarId);
-document.getElementById("btn-talkative").addEventListener("click", capturarId);
+document.getElementById("btn-hacker").addEventListener("click", capturarId)
+document.getElementById("btn-staff").addEventListener("click", capturarId)
+document.getElementById("btn-talkative").addEventListener("click", capturarId)
 
-btn_options.map((el)=>{
-    el.addEventListener('click', (evt)=>{
-        if (el.innerHTML == 'Falar com guarda') {
-            security1.addEventListener('click', (evt)=>{
-                if (Number(res_dices.innerHTML) >= 12 && card == 'btn-talkative') {
-                    balon_res.innerHTML='Security 1: Talvez tenha alguma coisa no armário!'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 18 && card == 'btn-hacker') {
-                    balon_res.innerHTML='Security 1: O que você quer?'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 16 && card == 'btn-staff') {
-                    balon_res.innerHTML='Security 1: Olá, como você vai?'
-                }
-                
-                else {
-                    balon_res.innerHTML = 'Tentativa falhou'
-                }
-            })
-
-            security2.addEventListener('click', (evt)=>{
-                if (Number(res_dices.innerHTML) >= 12 && card == 'btn-talkative') {
-                    balon_res.innerHTML='Security 2: Talvez tenha alguma coisa no armário!'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 18 && card == 'btn-hacker') {
-                    balon_res.innerHTML='Security 2: O que você quer?'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 16 && card == 'btn-staff') {
-                    balon_res.innerHTML='Security 2: Olá, como você vai?'
-                }
-                
-                else {
-                    balon_res.innerHTML = 'Tentativa falhou'
-                }
-            })
-
-            security3.addEventListener('click', (evt)=>{
-                if (Number(res_dices.innerHTML) >= 12 && card == 'btn-talkative') {
-                    balon_res.innerHTML='Security 3: Talvez tenha alguma coisa no armário!'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 18 && card == 'btn-hacker') {
-                    balon_res.innerHTML='Security 3: O que você quer?'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 16 && card == 'btn-staff') {
-                    balon_res.innerHTML='Security 3: Olá, como você vai?'
-                }
-                
-                else {
-                    balon_res.innerHTML = 'Tentativa falhou'
-                }
-            })
-        }
-
-        else if (el.innerHTML == 'Eliminar um guarda') {
-            security1.addEventListener('click', (evt)=>{
-                if (Number(res_dices.innerHTML) >= 12 && card == 'btn-staff') {
-                    if (alertShown) {
-                        alert('Security 1: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security1.style.visibility = 'hidden'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 16 && card == 'btn-talkative') {
-                    if (alertShown) {
-                        alert('Security 1: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security1.style.visibility = 'hidden'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 18 && card == 'btn-hacker') {
-                    if (alertShown) {
-                        alert('Security 1: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security1.style.visibility = 'hidden'
-                } 
-                
-                else {
-                    balon_res.innerHTML = 'Tentativa falhou'
-                }
-            })
-
-            security2.addEventListener('click', (evt)=>{
-                if (Number(res_dices.innerHTML) >= 12 && card == 'btn-staff') {
-                    if (alertShown) {
-                        alert('Security 2: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security2.style.visibility = 'hidden'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 16 && card == 'btn-talkative') {
-                    if (alertShown) {
-                        alert('Security 2: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security2.style.visibility = 'hidden'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 18 && card == 'btn-hacker') {
-                    if (alertShown) {
-                        alert('Security 2: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security2.style.visibility = 'hidden'
-                } 
-                
-                else {
-                    balon_res.innerHTML = 'Tentativa falhou'
-                }
-            })
-
-            security3.addEventListener('click', (evt)=>{
-                if (Number(res_dices.innerHTML) >= 12 && card == 'btn-staff') {
-                    if (alertShown) {
-                        alert('Security 3: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security3.style.visibility = 'hidden'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 16 && card == 'btn-talkative') {
-                    if (alertShown) {
-                        alert('Security 3: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security3.style.visibility = 'hidden'
-                }
-
-                else if (Number(res_dices.innerHTML) >= 18 && card == 'btn-hacker') {
-                    if (alertShown) {
-                        alert('Security 3: Eliminate!')
-                        alertShown = false
-
-                        setTimeout(function (){
-                            alertShown = true
-                        }, 2000)
-                    }
-                    security3.style.visibility = 'hidden'
-                } 
-                
-                else {
-                    balon_res.innerHTML = 'Tentativa falhou'
-                }
-            })
-        }
-
-        if (el.innerHTML == 'Entrar no vestiário' && (card == 'btn-staff' || card == 'btn-hacker' || card == 'btn-talkative')) {
-            if (security1.style.visibility == 'hidden' && security2.style.visibility == 'hidden' && security3.style.visibility == 'hidden') {
-                if (Number(res_dices.innerHTML) >= 18) {
-                    alert('Você entrou no vestiário!')
-                    // Armários
-                    armario1.addEventListener('click',()=>{
-                        if (Number(res_dices.innerHTML) >= 16) {
-                            armario1.src = '../../img/objetos/armario/armario-quebrado.png'
-                            if (alertShown) {
-                                alert('Ops! Nesse armário não tem nada.')
-                                alertShown = false
-
-                                setTimeout(function (){
-                                    alertShown = true
-                                }, 2000)
-                            }
-                        } else {
-                            balon_res.innerHTML='Wow! Your tentative failed!'
-                        }
-                    })
-
-                    armario2.addEventListener('click',()=>{
-                        if (Number(res_dices.innerHTML) >= 16) {
-                            armario2.src = '../../img/objetos/armario/armario-quebrado.png'
-                            if (alertShown) {
-                                alert('Parabéns! Você encontrou a roupa dos guardas.')
-                                alertShown = false
-
-                                setTimeout(function (){
-                                    alertShown = true
-                                }, 2000)
-                            }
-                        } else {
-                            balon_res.innerHTML='Wow! Your tentative failed!'
-                        }
-                    })
-                } else {
-                    alert('Tentativa falhou')
-                }
-            } else {
-                if (alertShown) {
-                    alert('Ops! Você ainda não pode executar essa ação.')
-                    alertShown = false
-    
-                    setTimeout(function (){
-                        alertShown = true
-                    }, 2000)
-                }
-            }
-        }
+// Seleciona a ação que o usuário deseja executar no game
+btn_options.map((el) => {
+    el.addEventListener('click', (evt) => {
+        currentAction = el.innerHTML
     })
 })
+
+// Ações dos personagens e valores dos dados
+const handleSecurityClick = (security, minFalar, minEliminar) => {
+    if (currentAction === 'Falar com guarda') {
+        if (Number(res_dices.innerHTML) >= minFalar) {
+            if (card === 'btn-talkative') {
+                if (security.id == 'security1') {
+                    balon_res.innerHTML = `Security 1: O que você quer?`
+                } else if (security.id == 'security2') {
+                    balon_res.innerHTML = `Security 2: Talvez tenha alguma coisa no armário!` // Talkative: único que contém essa função
+                } else if (security.id == 'security3') {
+                    balon_res.innerHTML = `Security 3: Olá, como você vai?`
+                }
+            } else if (card === 'btn-hacker' && Number(res_dices.innerHTML) >= 18) {
+                if (security.id == 'security1') {
+                    balon_res.innerHTML = `Security 1: O que você quer?`
+                } else if (security.id == 'security2') {
+                    balon_res.innerHTML = `Security 2: Talvez tenha alguma coisa no armário!`
+                } else if (security.id == 'security3') {
+                    balon_res.innerHTML = `Security 3: Olá, como você vai?`
+                } else {
+                    balon_res.innerHTML = `Tentativa falhou`
+                }
+            } else if (card === 'btn-staff' && Number(res_dices.innerHTML) >= 16) {
+                if (security.id == 'security1') {
+                    balon_res.innerHTML = `Security 1: O que você quer?`
+                } else if (security.id == 'security2') {
+                    balon_res.innerHTML = `Security 2: Talvez tenha alguma coisa no armário!`
+                } else if (security.id == 'security3') {
+                    balon_res.innerHTML = `Security 3: Olá, como você vai?`
+                } else {
+                    balon_res.innerHTML = `Tentativa falhou`
+                }
+            }
+        } else {
+            balon_res.innerHTML = 'Tentativa falhou'
+        }
+
+    } else if (currentAction === 'Eliminar um guarda') {
+        if ((card === 'btn-staff' && Number(res_dices.innerHTML) >= 12) ||
+            (card === 'btn-talkative' && Number(res_dices.innerHTML) >= 16) ||
+            (card === 'btn-hacker' && Number(res_dices.innerHTML) >= 18)) {
+            if (alertShown) {
+                if (security.id == 'security1') {
+                    alert(`Security 1: Eliminate!`)
+                } else if (security.id == 'security2') {
+                    alert(`Security 2: Eliminate!`)
+                } else if (security.id == 'security3') {
+                    alert(`Security 3: Eliminate!`)
+                }
+                alertShown = false
+                setTimeout(() => { alertShown = true }, 2000)
+            }
+            security.style.visibility = 'hidden'
+        } else {
+            balon_res.innerHTML = 'Tentativa falhou'
+        }
+    }
+}
+
+security1.addEventListener('click', () => handleSecurityClick(security1, 12, 12))
+security2.addEventListener('click', () => handleSecurityClick(security2, 12, 12))
+security3.addEventListener('click', () => handleSecurityClick(security3, 12, 12))
+
+const abrirArmario = () => {
+    if (currentAction === 'Abrir armário') {
+        if (security1.style.visibility == 'hidden' && security2.style.visibility == 'hidden' && security3.style.visibility == 'hidden') {
+            armario1.addEventListener('click',()=>{
+                if (Number(res_dices.innerHTML) >= 16) {
+                    armario1.src = '../../img/objetos/armario/armario-quebrado.png'
+                    if (alertShown) {
+                        alert('Ops! Nesse armário não tem nada.')
+                        alertShown = false
+
+                        setTimeout(function (){
+                            alertShown = true
+                        }, 2000)
+                    }
+                } else {
+                    balon_res.innerHTML='Wow! Your tentative failed!'
+                }
+            })
+
+            armario2.addEventListener('click',()=>{
+                if (Number(res_dices.innerHTML) >= 16) {
+                    armario2.src = '../../img/objetos/armario/armario-quebrado.png'
+                    if (alertShown) {
+                        alert('Parabéns! Vocês encontraram as roupas dos guardas.')
+                        alertShown = false
+
+                        setTimeout(function (){
+                            alertShown = true
+                        }, 2000)
+                    }
+                } else {
+                    balon_res.innerHTML='Wow! Your tentative failed!'
+                }
+            })
+            
+        } else {
+            alert('Você ainda não pode executar está ação!')
+        }
+    }
+}
+
+armario1.addEventListener('click', abrirArmario)
+armario2.addEventListener('click', abrirArmario)
 
 const msg = (mensagem) => {
     balon_res.style.visibility = 'visible'
@@ -288,13 +160,6 @@ const del_msg = () => {
 
 security1.addEventListener('mouseover', ()=>{
     msg('Security 1: Why so much noise? Why did not I choose to be a programmer?!')
-    // security1.addEventListener('click',()=>{
-    //     if (Number(res_dices.innerHTML) > 18 && card == 'btn-talkative') {
-    //         security1.style.visibility = 'hidden'
-    //     } else {
-    //         balon_res.innerHTML='Wow! Your tentative failed!'
-    //     }
-    // })
 })
 
 security1.addEventListener('mouseout', ()=>{
@@ -303,13 +168,6 @@ security1.addEventListener('mouseout', ()=>{
 
 security2.addEventListener('mouseover', ()=>{
     msg('Security 2: Where are my clothes?')
-    // security2.addEventListener('click',()=>{
-    //     if (Number(res_dices.innerHTML) > 18 && card == 'btn-talkative') {
-    //         security2.style.visibility = 'hidden'
-    //     } else {
-    //         balon_res.innerHTML='Wow! Your tentative failed!'
-    //     }
-    // })
 })
 
 security2.addEventListener('mouseout', ()=>{
@@ -318,13 +176,6 @@ security2.addEventListener('mouseout', ()=>{
 
 security3.addEventListener('mouseover', ()=>{
     msg('Security 3: Cronch! zzZ!')
-    // security3.addEventListener('click',()=>{
-    //     if (Number(res_dices.innerHTML) > 18 && card == 'btn-talkative') {
-    //         security3.style.visibility = 'hidden'
-    //     } else {
-    //         balon_res.innerHTML='Wow! Your tentative failed!'
-    //     }
-    // })
 })
 
 security3.addEventListener('mouseout', ()=>{
@@ -365,21 +216,6 @@ cliente2.addEventListener('mouseout', ()=>{
 
 armario1.addEventListener('mouseover', ()=>{
     msg('Armário 1')
-    // armario1.addEventListener('click',()=>{
-    //     if (Number(res_dices.innerHTML) > 16 && card == 'btn-staff') {
-    //         armario1.src = '../../img/objetos/armario/armario-quebrado.png'
-    //         if (alertShown) {
-    //             alert('Ops! Nesse armário não tem nada.')
-    //             alertShown = false
-
-    //             setTimeout(function (){
-    //                 alertShown = true
-    //             }, 2000)
-    //         }
-    //     } else {
-    //         balon_res.innerHTML='Wow! Your tentative failed!'
-    //     }
-    // })
 })
 
 armario1.addEventListener('mouseout', ()=>{
@@ -388,21 +224,6 @@ armario1.addEventListener('mouseout', ()=>{
 
 armario2.addEventListener('mouseover', ()=>{
     msg('Armário 2')
-    // armario2.addEventListener('click',()=>{
-    //     if (Number(res_dices.innerHTML) > 16 && card == 'btn-staff') {
-    //         armario2.src = '../../img/objetos/armario/armario-quebrado.png'
-    //         if (alertShown) {
-    //             alert('Congratulations! You have a shirts the guards.')
-    //             alertShown = false
-
-    //             setTimeout(function (){
-    //                 alertShown = true
-    //             }, 2000)
-    //         }
-    //     } else {
-    //         balon_res.innerHTML='Wow! Your tentative failed!'
-    //     }
-    // })
 })
 
 armario2.addEventListener('mouseout', ()=>{
